5e87a8640d29ddb00e7a3d0cac6209d5995f9160
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-05-18 12:13:21 +0200

    use pyhgtmap

--- assets/scripts/05_dem_contours1.sh
+++ assets/scripts/05_dem_contours1.sh
@@ -5,4 +5,7 @@ cd /mnt/data/srtm
 
 # create contour lines - this takes a long time
 # and you need A LOT of RAM, at least 16 GB are needed!
-phyghtmap -o contour --max-nodes-per-tile=0 -s 10 -0 --pbf warp-60.tif
+# phyghtmap -o contour --max-nodes-per-tile=0 -s 10 -0 --pbf warp-60.tif
+
+pyhgtmap --polygon europe.poly --step=10 --pbf --hgtdir=hgt  --source=view3 --simplifyContoursEpsilon=0.0001 -j16
+
